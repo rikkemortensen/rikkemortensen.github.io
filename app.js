5deacb4ecfaded9a50edcb3fa5290a45a68789c7
Rikke <rikkepeterzen@gmail.com>
2022-07-23 16:19:26 +0200

    animation removed

--- app.js
+++ app.js
@@ -1,14 +1,3 @@
-//Items
-const drone = document.querySelector(".drone img");
-
-//Moving Animation Event
-drone.addEventListener("mousemove", (e) => {
-    let xAxis = (window.innerWidth / 2 - e.pageX) / 12;
-    let yAxis = (window.innerHeight / 2 - e.pageY) / 12;
-    drone.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
-});
-
-
 const route = (event) => {
     event = event || window.event;
     event.preventDefault();
